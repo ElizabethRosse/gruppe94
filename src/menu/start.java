0f0ffc3d1e15674d89ab0cf099c91100467d9367
Jens Reiners <J.Reiners@hhu.de>
2013-05-16 10:05:59 +0200

    neue start Datei

---
+++ src/menu/start.java
@@ -0,0 +1,27 @@
+package menu;
+
+
+
+import javax.swing.JFrame;
+
+public class start extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
+
+	public start() {
+		add(new menu());
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Game");
+		setLocationRelativeTo(null);
+		//setSize(1000, 1000);
+		setResizable(false);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new start();
+
+	}
+
+}
