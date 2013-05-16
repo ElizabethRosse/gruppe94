888c8b39b2d263afa562f65fb0da6089513ce613
Jens Reiners <J.Reiners@hhu.de>
2013-05-16 10:05:04 +0200

    neue Tree Datei

---
+++ src/game/Tree.java
@@ -0,0 +1,39 @@
+package game;
+
+
+
+import java.awt.Image;
+import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
+
+public class Tree {
+	
+	private int x, y, width, height;
+	private Image image;
+	
+	public Tree(int x, int y) {
+		ImageIcon ii =
+				new ImageIcon(this.getClass().getResource("images/tree.gif"));
+		image = ii.getImage();
+		width = image.getWidth(null);   //lädt Image, setzt Variablen
+		height = image.getHeight(null);
+		this.x = x;                     
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	public Image getImage() {   
+		return image;
+	}
+	
+	public Rectangle getBounds() {  //for Collision detect
+		return new Rectangle(x, y, width, height);
+	}
+}
