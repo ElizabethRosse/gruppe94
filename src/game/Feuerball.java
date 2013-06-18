451ee037bc1253ba99fcd1143abd602a7381354e
Jens Reiners <J.Reiners@hhu.de>
2013-06-18 22:15:06 +0200

    anderer schadens wert

--- src/game/Feuerball.java
+++ src/game/Feuerball.java
@@ -18,7 +18,7 @@ public class Feuerball {
 		
 		ImageIcon ii = new ImageIcon(this.getClass().getResource("images/fball.png"));
 		image = ii.getImage();
-		damage = 5;
+		damage = 100;
 		visible = true;
 		width = image.getWidth(null);
 		height = image.getHeight(null);
