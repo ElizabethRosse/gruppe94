a4b947d378443b4c5be9995d1a0bde7af7a62e4e
Jens Reiners <J.Reiners@hhu.de>
2013-06-18 15:15:00 +0200

    added health and damage

--- src/game/Enemy.java
+++ src/game/Enemy.java
@@ -6,16 +6,39 @@ import javax.swing.ImageIcon;
 
 public class Enemy {
 
-	private int x, y, width, height;
+	private int x, y, width, height, damage;
+	private boolean visible = true;
+	private int life = 2;
 	private Image image;
 	
 	public Enemy(int x, int y){
 		ImageIcon ei = new ImageIcon(this.getClass().getResource("images/enemy.gif"));
 		image = ei.getImage();
+		damage = 1;
 		width = image.getWidth(null);
 		height = image.getHeight(null);
 		this.x = x;
 		this.y = y;
+	}
+	
+	public int getDmg() {
+		return damage;
+	}
+	
+	public int getLife (){
+		return life;
+	}
+	
+	public void damage (int dmg){
+		this.life -= dmg;
+	}
+	
+	public boolean isVisible () {
+		return visible;
+	}
+	
+	public void setVisible (boolean visible){
+		this.visible = visible;
 	}
 	
 	public int getX(){
