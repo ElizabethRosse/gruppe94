782fba0d6342f822ace1af7e921a043e18131e31
Jens Reiners <J.Reiners@hhu.de>
2013-07-04 08:40:40 +0200

    deleted useless stuff

--- src/game/Sounds.java
+++ src/game/Sounds.java
@@ -8,9 +8,7 @@ import javax.sound.sampled.Clip;
 //import sun.audio.ContinuousAudioDataStream;
 
 public class Sounds {
-	
-	    public Sounds() //static synchronized void play() //final String fileName, int muse 
-	    {}
+	   
 	        // Note: use .wav files
 	    	public static synchronized void play (int muse) {
 	    	switch(muse) {
