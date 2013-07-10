e518a42f974eb559589026973fb7eb98967b918c
David Bahr <david.bahr@hhu.de>
2013-07-10 10:56:14 +0200

    some sounds

--- src/game/Sounds.java
+++ src/game/Sounds.java
@@ -35,6 +35,31 @@ public class Sounds {
 	    				}
 	    				break;
 	    		}
+	    		case 3: {
+    				try {
+    					final Clip clip = AudioSystem.getClip();
+    					AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("src\\game\\sounds\\obtain.wav"));
+    					clip.open(inputStream);
+    					clip.start();
+    					
+    				} catch (Exception e) {
+    					System.out.println("play sound error: " + e.getMessage() + " for " + "obtain.wav");
+    				}
+    				break;
+	    		}	   
+	    		case 4: {
+    				try {
+    					final Clip clip = AudioSystem.getClip();
+    					AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("src\\game\\sounds\\shot.wav"));
+    					clip.open(inputStream);
+    					clip.start();
+    					
+    				} catch (Exception e) {
+    					System.out.println("play sound error: " + e.getMessage() + " for " + "shot.wav");
+    				}
+    				break;
+	    		}	 	    		
+	    		
     			}
 	    	
 	    	
