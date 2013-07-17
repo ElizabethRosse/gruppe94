f928eecf1acbaa97962020426d9e3082f00b19ec
Max Wiese <maximilian.wiese@hhu.de>
2013-07-18 00:05:57 +0200

    javadoc

--- src/game/shopkeeper.java
+++ src/game/shopkeeper.java
@@ -10,6 +10,11 @@ public class shopkeeper {
 	private Image image;
 	private boolean visible = true;
 	
+	/**
+	 * Erstelle Shopkeeper
+	 * @param x
+	 * @param y
+	 */
 	public shopkeeper(int x, int y) {
 		ImageIcon ii =
 				new ImageIcon(this.getClass().getResource("images/shoptyp.png"));
@@ -20,21 +25,42 @@ public class shopkeeper {
 		this.y = y;
 	}
 	
+	/**
+	 * Gebe X-Parameter des Shopkeepers aus
+	 * @return x
+	 */
 	public int getX() {
 		return x;
 	}
 	
+	/**
+	 * Gebe Sichtbarkeit des Shopkeepers aus
+	 * @return visible
+	 */
 	public boolean isVisible () {
 		return visible;
 	}
 	
+	/**
+	 * Gebe Y-Paramter des Shopkeepers aus
+	 * @return y
+	 */
 	public int getY() {
 		return y;
 	}
+	
+	/**
+	 * Gebe Bild des Shopkeepers aus
+	 * @return image
+	 */
 	public Image getImage() {   
 		return image;
 	}
 	
+	/**
+	 * Gebe Größe des Shopkeepers aus
+	 * @return x, y, breite, höhe
+	 */
 	public Rectangle getBounds() {  //for Collision detect
 		return new Rectangle(x, y, width, height);
 	}
