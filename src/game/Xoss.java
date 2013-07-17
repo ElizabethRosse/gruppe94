bfaed56e8e9800af59b34b7916529e0733f1a86a
Max Wiese <maximilian.wiese@hhu.de>
2013-07-18 00:03:57 +0200

    javadoc

--- src/game/Xoss.java
+++ src/game/Xoss.java
@@ -17,7 +17,12 @@ public class Xoss {
 	private boolean Arrow;
 	private ArrayList<Arrow> arrows = new ArrayList<Arrow>();
 	
-	
+	/**
+	 * Erstelle zweiten Boss
+	 * @param x
+	 * @param y
+	 * @param dir
+	 */
 	public Xoss(int x, int y, int dir){
 		ImageIcon ei = new ImageIcon(this.getClass().getResource("images/boss2.png"));
 		image = ei.getImage();
@@ -35,53 +40,101 @@ public class Xoss {
 		
 	}
 	
+	/**
+	 * Gebe Schaden des zweiten Bosses aus
+	 * @return damage
+	 */
 	public int getDmg() {
 		return damage;
 	}
 	
+	/**
+	 * Gebe Leben des zweiten Bosses aus
+	 * @return life
+	 */
 	public int getLife (){
 		return life;
 	}
 	
+	/**
+	 * Berechne Leben nach Schaden des zweiten Bosses
+	 */
 	public void damage (int dmg){
 		this.life -= dmg;
 	}
 	
+	/**
+	 * Gebe Sichtbarkeit des zweiten Bosses aus
+	 * @return visible
+	 */
 	public boolean isVisible () {
 		return visible;
 	}
 	
+	/**
+	 * Setze Sichtbarkeit des zweiten Bosses
+	 */
 	public void setVisible (boolean visible){
 		this.visible = visible;
 	}
 	
+	/**
+	 * Gebe X-Parameter des zweiten Bosses aus
+	 * @return x
+	 */
 	public int getX(){
 		return x;
 	}
 	
+	/**
+	 * Gebe Y-Parameter des zweiten Bosses aus
+	 * @return y
+	 */
 	public int getY(){
 		return y;
 	}
 
+	/**
+	 * Gebe Bild des zweiten Bosses aus
+	 * @return image
+	 */
 	public Image getImage(){
 		return image;
 	}
 	
+	/**
+	 * Gebe Größe des zweiten Bosses aus
+	 * @return x+15, y, 35, 35
+	 */
 	public Rectangle getBounds(){
 		return new Rectangle(x+15, y, 35, 35);
 	}
 	
+	/**
+	 * Gebe GrößeX des zweiten Bosses aus
+	 * @return x+10, y, 40, 50
+	 */
 	public Rectangle getBoundsX() {
 		return new Rectangle(x+10, y, 40, 50);
 	}
+	/**
+	 * Gebe Arraylist des zweiten Bosses aus
+	 * @return arrows
+	 */
 	public ArrayList<Arrow> getBossArrows() {
 		return arrows;
 	}
 	
+	/**
+	 * Erstelle neue Arrayliste des zweiten Bosses
+	 */
 	public void resArrows() {
 		arrows = new ArrayList<Arrow>();
 	}
 	
+	/**
+	 * Schieße Pfeile
+	 */
 	public void shoot() {
 		if (direction==1) arrows.add(new Arrow(x  +width  , y-3+height/2, direction));
 		else if (direction==2) arrows.add(new Arrow(x-2        , y-3+height/2, direction));
@@ -89,6 +142,11 @@ public class Xoss {
 		else if (direction==4) arrows.add(new Arrow(x-3+width/2, y           , direction));
 	}
 
+	/**
+	 * Ändere Bewegungsrichtung des zweiten Bosses
+	 * @param x
+	 * @param y
+	 */
 	public void changedirection(int x, int y) {
 		double h = Math.random();
 		/*if (h < 0.25) direction = 1;
@@ -111,11 +169,17 @@ public class Xoss {
 		else direction = 2;
 	}
 	
+	/**
+	 * Bewege zweiten Boss
+	 */
 	public void move() {
 		x = x+dx;
 		y = y+dy;
 	}
 	
+	/**
+	 * Ändere Bewegungsrichtung des zweiten Bosses
+	 */
 	public void movecollide() {
 		if (dx>0) {
 			direction = 2;
