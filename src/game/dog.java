299760de7e53c43724be886e22aff021bb448e34
Max Wiese <maximilian.wiese@hhu.de>
2013-07-18 00:05:31 +0200

    javadoc

--- src/game/dog.java
+++ src/game/dog.java
@@ -13,7 +13,11 @@ public class dog {
 	private Image image;
 
 	
-	
+	/**
+	 * Erstelle Hund
+	 * @param x
+	 * @param y
+	 */
 	public dog(int x, int y){
 		ImageIcon ei = new ImageIcon(this.getClass().getResource("images/dog.png"));
 		image = ei.getImage();
@@ -23,27 +27,50 @@ public class dog {
 		this.y = y;
 	}
 	
-	
+	/**
+	 * Gebe Sichtbarkeit des Hundes saus
+	 * @return visible
+	 */
 	public boolean isVisible () {
 		return visible;
 	}
 	
+	/**
+	 * Setze SIhtbarkeit des Hundes
+	 * @param visible
+	 */
 	public void setVisible (boolean visible){
 		this.visible = visible;
 	}
 	
+	/**
+	 * Gebe X-Paramter des Hundes aus
+	 * @return x
+	 */
 	public int getX(){
 		return x;
 	}
 	
+	/**
+	 * Gebe Y-Paramter des Hundes aus
+	 * @return y
+	 */
 	public int getY(){
 		return y;
 	}
 	
+	/**
+	 * Gebe Bild des Hundes aus
+	 * @return image
+	 */
 	public Image getImage(){
 		return image;
 	}
 	
+	/**
+	 * Gebe Größe des Hunde aus
+	 * @return x, y, breite, höhe
+	 */
 	public Rectangle getBounds(){
 		return new Rectangle(x, y, width, height);
 	}
