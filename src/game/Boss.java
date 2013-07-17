5a3c7ec5b77f41ef583cd9a34fadb1553c4587f3
Max Wiese <maximilian.wiese@hhu.de>
2013-07-18 00:03:52 +0200

    javadoc

--- src/game/Boss.java
+++ src/game/Boss.java
@@ -12,6 +12,12 @@ public class Boss {
 	private int life = 500;
 	private Image image;
 	
+	/**
+	 * Erstelle den ersten Boss
+	 * @param x
+	 * @param y
+	 * @param direction
+	 */
 	public Boss(int x, int y, int dir){
 		ImageIcon ei = new ImageIcon(this.getClass().getResource("images/boss1.png"));
 		image = ei.getImage();
@@ -23,46 +29,89 @@ public class Boss {
 		dy = 0;
 	}
 	
+	/**
+	 * Gebe Schaden des ersten Bosses aus
+	 * @return damage
+	 */
 	public int getDmg() {
 		return damage;
 	}
 	
+	/**
+	 * Gebe Leben des ersten Bosses aus
+	 * @return life
+	 */
 	public int getLife (){
 		return life;
 	}
 	
+	/**
+	 * Ziehe Schaden vom Leben des ersten Bosses ab
+	 * @param dmg
+	 */
 	public void damage (int dmg){
 		this.life -= dmg;
 	}
 	
+	/**
+	 * Gebe Sichtbarkeit des ersten Bosses aus
+	 * @return visible
+	 */
 	public boolean isVisible () {
 		return visible;
 	}
 	
+	/**
+	 * Setze Sichtbarkeit des ersten Bosses
+	 * @param visible
+	 */
 	public void setVisible (boolean visible){
 		this.visible = visible;
 	}
 	
+	/**
+	 * Gebe X-Paramter des ersten Bosses aus
+	 * @return x
+	 */
 	public int getX(){
 		return x;
 	}
 	
+	/**
+	 *  Gebe Y-Paramter des ersten Bosses aus
+	 * @return y
+	 */
 	public int getY(){
 		return y;
 	}
 
+	/**
+	 * Gebe Bild des ersten Bosses aus
+	 * @return image
+	 */
 	public Image getImage(){
 		return image;
 	}
 	
+	/**
+	 * Gebe Größe des ersten Bosses aus
+	 * @return x, y
+	 */
 	public Rectangle getBounds(){
 		return new Rectangle(x, y, 35, 35);
 	}
+	
+	/**
+	 * Bewege den ersten Boss
+	 */
 	public void move() {
 		x = x+dx;
 		y = y+dy;
 	}
 	
+	/**
+	 * Ändere Richtung bei Kollision mit einem Objekt
+	 */
 	public void movecollide() {
 		if (dx>0) {
 			x = x-3;
