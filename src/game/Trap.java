8e68b1fcfe21b0793ce194fe85a543ee775b5549
Max Wiese <maximilian.wiese@hhu.de>
2013-07-18 00:03:42 +0200

    javadoc

--- src/game/Trap.java
+++ src/game/Trap.java
@@ -11,6 +11,11 @@ public class Trap {
 	private boolean visible = true;
 	private Image image;
 	
+	/**
+	 * Erstelle Falle
+	 * @param x
+	 * @param y
+	 */
 	public Trap(int x, int y){
 		ImageIcon ei = new ImageIcon(this.getClass().getResource("images/trap.png"));
 		image = ei.getImage();
@@ -21,31 +26,59 @@ public class Trap {
 		this.y = y;
 	}
 	
+	/**
+	 * Gebe Schaden der Falle aus
+	 * @return damage
+	 */
 	public int getDmg() {
 		return damage;
 	}
 	
 	
+	/**
+	 * Gebe Sichtbarkeit der Falle aus
+	 * @return visible
+	 */
 	public boolean isVisible () {
 		return visible;
 	}
 	
+	/**
+	 * Setze Sichtbarkeit der Falle
+	 * @return visible
+	 */
 	public void setVisible (boolean visible){
 		this.visible = visible;
 	}
 	
+	/**
+	 * Gebe X-Parameter der Falle aus
+	 * @return x
+	 */
 	public int getX(){
 		return x;
 	}
 	
+	/**
+	 * Gebe Y-Parameter der Falle aus
+	 * @return y
+	 */
 	public int getY(){
 		return y;
 	}
 	
+	/**
+	 * Gebe Bild der Falle aus
+	 * @return image
+	 */
 	public Image getImage(){
 		return image;
 	}
 	
+	/**
+	 * Gebe Größe der Falle aus
+	 * @return x, y, breite, höhe
+	 */
 	public Rectangle getBounds(){
 		return new Rectangle(x, y, width, height);
 	}
