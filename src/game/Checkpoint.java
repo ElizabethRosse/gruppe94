ef3c3437357ceaeac8c05bcbb45062cc323f3bee
Max Wiese <maximilian.wiese@hhu.de>
2013-07-18 00:03:09 +0200

    javadoc

--- src/game/Checkpoint.java
+++ src/game/Checkpoint.java
@@ -9,6 +9,11 @@ public class Checkpoint {
 	private Image imageac, imagein;
 	private boolean activated = false;
 	
+	/**
+	 * Erstelle Checkpoint
+	 * @param x
+	 * @param y
+	 */
 	public Checkpoint(int x, int y){
 		ImageIcon ei;									//different images for activated and deactivated
 			ei = new ImageIcon(this.getClass().getResource("images/checkpoint_activated.gif"));
@@ -22,35 +27,76 @@ public class Checkpoint {
 		this.x = x;
 		this.y = y;
 	}
+	
+	/**
+	 * Setze Aktivierung des Checkpoints
+	 * @param vis
+	 */
 	public void setActivated(boolean vis) {						//changes state of checkpoint
 		this.activated = vis;
 	}
 	
+	/**
+	 * Gebe X-Parameter des Checkpoints aus
+	 * @return
+	 */
 	public int getX(){
 		return x;
 	}
 	
+	/**
+	 * Gebe Y-Parameter des Checkpoint aus
+	 * @return
+	 */
 	public int getY(){
 		return y;
 	}
+	
+	/**
+	 * Gebe Map zurück
+	 * @return map
+	 */
 	public int getMap(){
 		return map;
 	}
+	
+	/**
+	 * Setze Map fest
+	 * @param i
+	 */
 	public void setMap(int i){
 		map = i;
 		
 	}
+	
+	/**
+	 * Gebe Bild des aktiven Checkpoints aus
+	 * @return imageac
+	 */
 	public Image getImageac(){
 		return imageac;
 	}
 	
+	/**
+	 * Gebe Bild des inaktiven Checkpoints aus
+	 * @return imagein
+	 */
 	public Image getImagein() {
 		return imagein;
 	}
 	
+	/**
+	 * Gebe Größe des Checkpoint aus
+	 * @return x, y, breite, höhe
+	 */
 	public Rectangle getBounds(){
 		return new Rectangle(x, y, width, height);
 	}
+	
+	/**
+	 * Gebe aus ob aktiviert
+	 * @return activated
+	 */
 	public boolean active() {								//gives back activated if activated and deactivated if deactivated
 		return activated;
 	}
